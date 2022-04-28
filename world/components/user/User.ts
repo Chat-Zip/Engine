19ffff33d1a0e9203d00ab6ab482b1eebcb7cc94
rdg1029 <wcle3456@gmail.com>
2022-04-28 18:13:26 +0900

    Update User : Inherit UserModel

--- world/components/user/User.ts
+++ world/components/user/User.ts
@@ -1,14 +1,17 @@
+import UserModel from "./model";
+
 export interface UserData {
-    id: string;
+    userId: string;
     name: string;
 }
 
-export default class User implements UserData {
-    id: string;
+export default class User extends UserModel implements UserData {
+    userId: string;
     name: string;
 
     constructor(id: string, name: string) {
-        this.id = id;
+        super(name);
+        this.userId = id;
         this.name = name;
     }
 }
