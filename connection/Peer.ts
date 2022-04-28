f2eab7ee62064268485e9d3b5d39b7af111c592a
rdg1029 <wcle3456@gmail.com>
2022-04-28 16:12:16 +0900

    Add Peer : Init

---
+++ connection/Peer.ts
@@ -0,0 +1,55 @@
+export default class Peer extends RTCPeerConnection {
+    public chat: RTCDataChannel;
+    public movement: RTCDataChannel;
+
+    constructor() {
+        const iceConfig = {
+            iceServers : [
+                {
+                    urls: 'stun:stun1.l.google.com:19302'
+                }
+            ]
+        };
+        super(iceConfig);
+
+        this.chat = this.createDataChannel('chat', {negotiated: true, id: 0});
+        this.movement = this.createDataChannel('move', {negotiated: true, id: 1});
+        this.movement.binaryType = 'arraybuffer';
+    }
+
+    public createSessionDescription(type: 'answer'|'offer') {
+        return new Promise(resolve => {
+            this.onicegatheringstatechange = e => {
+                if (this.iceGatheringState !== 'complete') return;
+                resolve(this.localDescription);
+            }
+            switch (type) {
+                case 'offer':
+                    this.createOffer().then(offer => this.setLocalDescription(offer));
+                    break;
+                case 'answer':
+                    this.createAnswer().then(answer => this.setLocalDescription(answer));
+                    break;
+            }
+        });
+    }
+
+    public sendChat(data: string) {
+        const {chat} = this;
+        if (chat.readyState !== 'open') return
+        chat.send(data);
+    }
+
+    public sendMovement(data: ArrayBuffer) {
+        const {movement} = this;
+        if (movement.readyState !== 'open') return;
+        movement.send(data);
+    }
+
+    public close() {
+        const {chat, movement} = this
+        chat.close();
+        movement.close();
+        super.close();
+    }
+}
