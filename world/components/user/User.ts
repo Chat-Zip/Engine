89c718e086ce968cba81fa60a931b120fe408485
Caret <wcle3456@gmail.com>
2022-04-29 13:27:16 +0900

    Update User : Add updateMovement()

--- world/components/user/User.ts
+++ world/components/user/User.ts
@@ -1,3 +1,4 @@
+import { Vector3 } from "three";
 import UserModel from "./model";
 
 export interface UserData {
@@ -6,12 +7,28 @@ export interface UserData {
 }
 
 export default class User extends UserModel implements UserData {
+    private prevPos: Vector3;
+    private targetPos: Vector3;
+    private alpha: number;
+
     userId: string;
     name: string;
 
     constructor(id: string, name: string) {
         super(name);
+        this.prevPos = new Vector3();
+        this.targetPos = new Vector3();
+        this.alpha = 0;
+
         this.userId = id;
         this.name = name;
+    }
+
+    public updateMovement(buffer: ArrayBuffer) {
+        const { prevPos, targetPos } = this;
+        const mvArr = new Float32Array(buffer);
+        prevPos.copy(targetPos);
+        targetPos.fromArray(mvArr);
+        this.alpha = 0;
     }
 }
