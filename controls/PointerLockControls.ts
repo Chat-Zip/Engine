dbc596419b645ff38565e5c4aa75b7b06ed154f3
rdg1029 <wcle3456@gmail.com>
2022-04-30 15:05:59 +0900

    Add PointerLockControls : Init

---
+++ controls/PointerLockControls.ts
@@ -0,0 +1,100 @@
+import { Euler, EventDispatcher, Vector3, Camera } from "three";
+import Self from "../world/components/user/Self";
+
+const _euler = new Euler( 0, 0, 0, 'YXZ' );
+const _vector = new Vector3();
+
+const _changeEvent = { type: 'change' };
+const _lockEvent = { type: 'lock' };
+const _unlockEvent = { type: 'unlock' };
+
+const _PI_2 = Math.PI / 2;
+
+export default class PointerLockControls extends EventDispatcher {
+    camera: Camera;
+    displacement: Vector3;
+    domElement: HTMLCanvasElement;
+    isLocked: boolean;
+    pointerSpeed: number;
+
+    constructor(self: Self, domElement: HTMLCanvasElement) {
+        super();
+
+        this.camera = self.camera;
+        this.displacement = new Vector3().fromArray(self.state.pos);
+
+        this.domElement = domElement;
+        this.isLocked = false;
+
+        this.pointerSpeed = 1.0;
+    }
+
+    private onMouseMove(event: MouseEvent) {
+        const { camera, isLocked, pointerSpeed } = this;
+        if (!isLocked) return;
+
+        const movementX = event.movementX || 0;
+        const movementY = event.movementY || 0;
+
+        _euler.setFromQuaternion(camera.quaternion);
+
+        _euler.x -= movementY * 0.002 * pointerSpeed;
+        _euler.y -= movementX * 0.002 * pointerSpeed;
+
+        _euler.x = Math.max(-_PI_2, Math.min(_PI_2, _euler.x));
+
+        camera.quaternion.setFromEuler(_euler);
+
+        this.dispatchEvent(_changeEvent);
+    }
+
+    private onPointerLockChange() {
+        const { domElement } = this;
+        if (domElement.ownerDocument.pointerLockElement === domElement) {
+            this.dispatchEvent(_lockEvent);
+            this.isLocked = true;
+        } else {
+            this.dispatchEvent(_unlockEvent);
+            this.isLocked = false;
+        }
+    }
+
+    private onPointerLockError() {
+        console.error( 'Unable to use Pointer Lock API' );
+    }
+
+    public connect() {
+        const ownerDocument = this.domElement.ownerDocument;
+        ownerDocument.addEventListener('mousemove', this.onMouseMove);
+        ownerDocument.addEventListener('pointerlockchange', this.onPointerLockChange);
+        ownerDocument.addEventListener('pointerlockerror', this.onPointerLockError);
+    }
+
+    public disconnect() {
+        const ownerDocument = this.domElement.ownerDocument;
+        ownerDocument.addEventListener('mousemove', this.onMouseMove);
+        ownerDocument.addEventListener('pointerlockchange', this.onPointerLockChange);
+        ownerDocument.addEventListener('pointerlockerror', this.onPointerLockError);
+    }
+
+    public moveForward(distance: number) {
+        const { camera, displacement } = this;
+        _vector.setFromMatrixColumn(camera.matrix, 0);
+        _vector.crossVectors(camera.up, _vector);
+        displacement.addScaledVector(_vector, distance);
+    }
+
+    public moveRight(distance: number) {
+        const { camera, displacement } = this;
+        _vector.setFromMatrixColumn(camera.matrix, 0);
+        displacement.addScaledVector(_vector, distance);
+    }
+
+    public lock() {
+        this.domElement.requestPointerLock();
+    }
+
+    public unlock() {
+        this.domElement.ownerDocument.exitPointerLock();
+    }
+}
