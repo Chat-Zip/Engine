26a0a069cc631819fbd4ea4138a985f01f6a0f69
rdg1029 <wcle3456@gmail.com>
2022-05-04 13:42:46 +0900

    Update UserAppearance : Edit constructor

--- world/components/user/model/UserAppearance.ts
+++ world/components/user/model/UserAppearance.ts
@@ -1,14 +1,15 @@
-import { Texture, TextureLoader, SpriteMaterial, Sprite } from 'three';
+import { Texture, TextureLoader, SpriteMaterial, Sprite, NearestFilter } from 'three';
 
 export default class UserAppearance extends Sprite {
     map: Texture;
 
     constructor() {
         const map = new TextureLoader().load('./person.png');
+        map.magFilter = NearestFilter;
         const material = new SpriteMaterial({map: map});
         super(material);
-        this.position.y = 6;
-        this.scale.set(18, 18, 18);
+        this.position.y = 8;
+        this.scale.set(16, 16, 1);
         this.map = map;
         this.material = material;
     }
