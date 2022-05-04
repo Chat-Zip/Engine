3a3f3d817166fba1b18badbbf25b8e4442726c07
rdg1029 <wcle3456@gmail.com>
2022-05-04 18:12:53 +0900

    Update WorldMap : Replace map data to world data

--- world/components/map/index.ts
+++ world/components/map/index.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import JSZip from "jszip";
+import WORLD_DATA, { WorldData } from "../../WorldData";
 import Palette from "./Palette";
 
 const CHUNK_SIZE = 32;
@@ -77,101 +78,19 @@ const FACES = [
 
 const _material = new THREE.MeshLambertMaterial({vertexColors: true});
 
-interface MapData {
-    intensity: number;
-    paletteColors: Array<string>;
-    spawnPoint: Array<number>;
-}
-
 export default class WorldMap {
     private scene: THREE.Scene;
-    public data: MapData;
+    private data: WorldData;
     public chunks: Map<string, Uint8Array>;
     public meshs: Map<string, THREE.Mesh>;
-    public light: THREE.HemisphereLight;
     public palette: Palette;
 
     constructor(scene: THREE.Scene) {
         this.scene = scene;
-        this.data = {
-            intensity: 1,
-            paletteColors: [
-                "",
-                "#060608",
-                "#141013",
-                "#3b1725",
-                "#73172d",
-                "#b4202a",
-                "#df3e23",
-                "#fa6a0a",
-                "#f9a31b",
-                "#ffd541",
-                "#fffc40",
-                "#d6f264",
-                "#9cdb43",
-                "#59c135",
-                "#14a02e",
-                "#1a7a3e",
-                "#24523b",
-                "#122020",
-                "#143464",
-                "#285cc4",
-                "#249fde",
-                "#20d6c7",
-                "#a6fcdb",
-                "#ffffff",
-                "#fef3c0",
-                "#fad6b8",
-                "#f5a097",
-                "#e86a73",
-                "#bc4a9b",
-                "#793a80",
-                "#403353",
-                "#242234",
-                "#221c1a",
-                "#322b28",
-                "#71413b",
-                "#bb7547",
-                "#dba463",
-                "#f4d29c",
-                "#dae0ea",
-                "#b3b9d1",
-                "#8b93af",
-                "#6d758d",
-                "#4a5462",
-                "#333941",
-                "#422433",
-                "#5b3138",
-                "#8e5252",
-                "#ba756a",
-                "#e9b5a3",
-                "#e3e6ff",
-                "#b9bffb",
-                "#849be4",
-                "#588dbe",
-                "#477d85",
-                "#23674e",
-                "#328464",
-                "#5daf8d",
-                "#92dcba",
-                "#cdf7e2",
-                "#e4d2aa",
-                "#c7b08b",
-                "#a08662",
-                "#796755",
-                "#5a4e44",
-                "#423934"
-            ],
-            spawnPoint: [undefined, undefined, undefined],
-        };
+        this.data = WORLD_DATA;
         this.chunks = new Map();
         this.meshs = new Map();
-        this.light = new THREE.HemisphereLight(0xffffff, 0xaaaaaa, this.data.intensity);
         this.palette = new Palette(this.data.paletteColors);
-
-        this.light.matrixAutoUpdate = false;
-
-        this.scene.add(this.light);
     }
 
     public computeChunkId(x: number, y: number, z: number) {
