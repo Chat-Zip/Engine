75741d42958a834e35a2aa1a28d80d41696d560d
rdg1029 <wcle3456@gmail.com>
2022-05-07 11:10:24 +0900

    Update UserAppearance : Change sprite to plane mesh

--- world/components/user/model/UserAppearance.ts
+++ world/components/user/model/UserAppearance.ts
@@ -1,20 +1,19 @@
-import { Texture, TextureLoader, SpriteMaterial, Sprite, NearestFilter } from 'three';
+import { Texture, TextureLoader, PlaneGeometry, MeshBasicMaterial, Mesh, NearestFilter, Plane } from 'three';
 
-export default class UserAppearance extends Sprite {
-    map: Texture;
+export default class UserAppearance extends Mesh {
+    private map: Texture;
 
     constructor() {
         const map = new TextureLoader().load('./person.png');
         map.magFilter = NearestFilter;
-        const material = new SpriteMaterial({map: map});
-        super(material);
-        this.position.y = 8;
-        this.scale.set(16, 16, 1);
+        const plane = new PlaneGeometry(16, 16);
+        const material = new MeshBasicMaterial({map: map, alphaTest: 0.5});
+        super(plane, material);
         this.map = map;
-        this.material = material;
+        this.position.y += 8;
     }
     dispose() {
         this.map.dispose();
-        this.material.dispose();
+        this.geometry.dispose();
     }
 }
