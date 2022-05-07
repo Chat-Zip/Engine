57ba30d0e64229ca927cc4ba18a325de7cca4350
rdg1029 <wcle3456@gmail.com>
2022-05-07 13:21:42 +0900

    Add Skybox : Init

---
+++ world/components/Skybox.ts
@@ -0,0 +1,30 @@
+import { CubeTexture, CubeTextureLoader } from "three";
+
+const _loader = new CubeTextureLoader();
+
+export default class Skybox {
+    public texture: CubeTexture;
+    public images: Array<string>;
+
+    load(imgUrls: Array<string>) {
+        if (imgUrls.length !== 6) {
+            console.error('Only need 6 images to make a skybox.');
+            return;
+        }
+        return new Promise((resolve, reject) => {
+            this.images = imgUrls;
+            this.texture = _loader.load(
+                imgUrls,
+                () => {
+                    this.texture.matrixAutoUpdate = false;
+                    this.texture.updateMatrix();
+                    resolve(null);
+                },
+                null,
+                (e) => {
+                    reject(e);
+                }
+            );
+        });
+    }
+}
