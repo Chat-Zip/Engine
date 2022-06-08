3b9c3720fbb8d66b9dff83b5b3f83897d19f83e2
rdg1029 <wcle3456@gmail.com>
2022-06-08 13:26:12 +0900

    Remove Socket.ts

--- connection/Socket.ts
+++
@@ -1,9 +0,0 @@
-import io from 'socket.io-client';
-/*
-Local Test : http://localhost:3000
-Server Test : https://chatzip-signalling-server.herokuapp.com
-*/
-export default io('https://chatzip-signalling-server.herokuapp.com', { 
-    transports: ["websocket"],
-    closeOnBeforeunload: false
-});
