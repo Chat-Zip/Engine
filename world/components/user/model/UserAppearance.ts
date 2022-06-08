9f9ce6d901097c8feda16748ab65621e4c186328
rdg1029 <wcle3456@gmail.com>
2022-06-08 13:15:19 +0900

    Update UserAppearance : Edit constructor

--- world/components/user/model/UserAppearance.ts
+++ world/components/user/model/UserAppearance.ts
@@ -1,10 +1,10 @@
-import { Texture, TextureLoader, PlaneGeometry, MeshBasicMaterial, Mesh, NearestFilter, Plane } from 'three';
+import { Texture, TextureLoader, PlaneGeometry, MeshBasicMaterial, Mesh, NearestFilter } from 'three';
 
 export default class UserAppearance extends Mesh {
     private map: Texture;
 
-    constructor() {
-        const map = new TextureLoader().load('./person.png');
+    constructor(avatar: string) {
+        const map = new TextureLoader().load(avatar);
         map.magFilter = NearestFilter;
         const plane = new PlaneGeometry(16, 16);
         const material = new MeshBasicMaterial({map: map, alphaTest: 0.5});
