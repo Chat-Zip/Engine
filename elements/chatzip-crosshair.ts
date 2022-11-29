4cf4d30e41878bc560e44a3f24d9d318c894f9ea
rdg1029 <wcle3456@gmail.com>
2022-11-29 15:36:44 +0900

    Add chatzip-crosshair : Init

---
+++ elements/chatzip-crosshair.ts
@@ -0,0 +1,41 @@
+import { LitElement, html, css, CSSResultGroup, PropertyValueMap } from "lit";
+import { customElement, query } from "lit/decorators.js";
+
+import CROSS_HAIR from "../assets/crosshair.svg";
+
+@customElement('chatzip-crosshair')
+export class ChatZipCrossHair extends LitElement {
+
+    @query('#crosshair') _crosshair!: HTMLImageElement;
+
+    static styles?: CSSResultGroup = css`
+        #crosshair {
+            position: absolute;
+            width: 1em;
+            height: 1em;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 1;
+        }
+    `;
+
+    constructor() {
+        super();
+    }
+
+    protected render() {
+        return html`
+            <img id="crosshair" />
+        `;
+    }
+    protected firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+        this._crosshair.src = CROSS_HAIR;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "chatzip-crosshair": ChatZipCrossHair;
+    }
+}
