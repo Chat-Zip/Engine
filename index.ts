e2930c4ca64e309c8281d73deed6189fc19312ef
rdg1029 <wcle3456@gmail.com>
2024-10-07 11:06:00 +0900

    Update Engine : Apply enableEditor()

--- index.ts
+++ index.ts
@@ -2,21 +2,22 @@ import { Clock } from "three";
 import World from "./world";
 import Controls from "./controls/Controls";
 import Renderer from "./Renderer";
+import PointerControls from "./controls/PointerControls";
 
 const clock = new Clock();
 const TICK = 0.1;
 let duration = 0;
 
-class Engine {
+export class Engine {
     public world: World;
-    public controls: Controls;
+    public controls: Controls | undefined;
     public renderer: Renderer | undefined;
 
     public tickUpdate: boolean;
 
     constructor() {
         this.world = new World();
-        this.controls = this.world.self.controls;
+        this.controls = undefined;
         this.renderer = undefined;
         this.tickUpdate = true;
     }
@@ -37,6 +38,21 @@ class Engine {
             camera.aspect = width / height;
             camera.updateProjectionMatrix();
         });
+    }
+
+    public enableEditor(enable: boolean) {
+        const { world, controls, renderer } = this;
+        if (!(controls instanceof PointerControls)) return;
+        if (enable) {
+            controls?.addEventListener('lock', () => {
+                renderer?.domElement.addEventListener('pointerdown', world.editor.placeVoxel);
+                controls.addEventListener('change', world.editor.selectVoxel);
+            });
+            controls.addEventListener('unlock', () => {
+                renderer?.domElement.removeEventListener('pointerdown', world.editor.placeVoxel);
+                controls.removeEventListener('change', world.editor.selectVoxel);
+            })
+        }
     }
 
     public start() {
