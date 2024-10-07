2b6eb94301778bf254f6fdcd7796685386980b9a
rdg1029 <wcle3456@gmail.com>
2024-10-07 16:19:16 +0900

    Update chatzip-renderer : Apply attribute (controls)

--- elements/chatzip-renderer.ts
+++ elements/chatzip-renderer.ts
@@ -1,14 +1,13 @@
 import { LitElement, html, css, CSSResultGroup, PropertyValueMap } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
 import engine from '..';
-import PointerControls from '../controls/PointerControls';
-import eventKeyListeners from "../controls/KeyEventListeners";
 
 @customElement('chatzip-renderer')
 export class ChatZipRenderer extends LitElement {
 
     @query('#canvas') _canvas!: HTMLCanvasElement;
     @property({type: Boolean, attribute: 'enable-editor'}) enableEditor: Boolean = false;
+    @property({type: String, attribute: 'controls'}) controls: String = 'pointer';
 
     static styles?: CSSResultGroup = css`
         #canvas {
@@ -41,28 +40,9 @@ export class ChatZipRenderer extends LitElement {
     protected firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
         console.log('chatzip-renderer: first updated', this._canvas);
         const { _canvas } = this;
-        const pointerControls = new PointerControls(engine.world.self, _canvas);
 
         engine.setCanvasToRenderer(_canvas);
-        engine.setControls(pointerControls);
-        _canvas.addEventListener('click', e => {
-            pointerControls.lock();
-            pointerControls.isLocked = true;
-        });
-
-        const movements = engine.controls.movements;
-        const movKey = eventKeyListeners.move;
-        movKey.set('KeyW', (isDown: boolean) => movements.set('forward', isDown));
-        movKey.set('ArrowUp', (isDown: boolean) => movements.set('forward', isDown));
-        movKey.set('KeyS', (isDown: boolean) => movements.set('back', isDown));
-        movKey.set('ArrowDown', (isDown: boolean) => movements.set('back', isDown));
-        movKey.set('KeyA', (isDown: boolean) => movements.set('left', isDown));
-        movKey.set('ArrowLeft', (isDown: boolean) => movements.set('left', isDown));
-        movKey.set('KeyD', (isDown: boolean) => movements.set('right', isDown));
-        movKey.set('ArrowRight', (isDown: boolean) => movements.set('right', isDown));
-        movKey.set('Space', (isDown: boolean) => movements.set('top', isDown));
-        movKey.set('ShiftLeft', (isDown: boolean) => movements.set('down', isDown));
-
+        engine.setControls(this.controls as 'pointer' | 'touch');
         if (this.enableEditor) engine.enableEditor(true);
         engine.start();
     }
