6554c3bb3a7f88c590ccd1e4131976c73ca1b5e0
rdg1029 <wcle3456@gmail.com>
2024-10-08 21:24:44 +0900

    Update chatzip-renderer : Edit html & style & firstUpdated()

--- elements/chatzip-renderer.ts
+++ elements/chatzip-renderer.ts
@@ -2,21 +2,17 @@ import { LitElement, html, css, CSSResultGroup, PropertyValueMap } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
 import engine from '..';
 
+import '../elements/chatzip-crosshair';
+import '../elements/chatzip-palette';
+import '../elements/chatzip-world-file-manager';
+
 @customElement('chatzip-renderer')
 export class ChatZipRenderer extends LitElement {
 
+    @query('#render-frame') _render_frame!: HTMLDivElement;
     @query('#canvas') _canvas!: HTMLCanvasElement;
-    @property({type: Boolean, attribute: 'enable-editor'}) enableEditor: Boolean = false;
-    @property({type: String, attribute: 'controls'}) controls: String = 'pointer';
-
-    static styles?: CSSResultGroup = css`
-        #canvas {
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            z-index: 0;
-        }
-    `;
+    @property({ type: Boolean, attribute: 'enable-editor' }) enableEditor: Boolean = false;
+    @property({ type: String, attribute: 'controls' }) controls: String = 'pointer';
 
     constructor() {
         super();
@@ -34,16 +30,35 @@ export class ChatZipRenderer extends LitElement {
 
     protected render() {
         return html`
-            <canvas id="canvas"></canvas>
+            <style>
+                #render-frame {
+                    position: relative;
+                    width: 960px;
+                    height: 540px;
+                }
+                #canvas {
+                    position: absolute;
+                    z-index: 0;
+                }
+            </style>
+            <div id="render-frame">
+                <canvas id="canvas"></canvas>
+                <chatzip-crosshair></chatzip-crosshair>
+                <chatzip-palette></chatzip-palette>
+                <chatzip-world-file-manager></chatzip-world-file-manager>
+            </div>
         `;
     }
     protected firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
-        console.log('chatzip-renderer: first updated', this._canvas);
-        const { _canvas } = this;
-
+        console.log('chatzip-renderer: first updated', this.renderRoot);
+        const { _render_frame, _canvas } = this;
         engine.setCanvasToRenderer(_canvas);
         engine.setControls(this.controls as 'pointer' | 'touch');
-        if (this.enableEditor) engine.enableEditor(true);
+        engine.enableEditor(this.enableEditor ? true : false);
+        new ResizeObserver(entries => {
+            const {width, height} = entries[0].contentRect;
+            engine.renderer?.setSize(width, height);
+        }).observe(_render_frame);
         engine.start();
     }
     protected createRenderRoot(): Element | ShadowRoot {
