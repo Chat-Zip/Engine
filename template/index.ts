0ed80a612af3afd4d7acc0c4207d2fd20ef1375d
rdg1029 <wcle3456@gmail.com>
2024-10-08 21:25:37 +0900

    Update template page

--- template/index.ts
+++ template/index.ts
@@ -1,7 +1,4 @@
 import '../elements/chatzip-renderer';
-import '../elements/chatzip-crosshair';
-import '../elements/chatzip-palette';
-import '../elements/chatzip-world-file-manager';
 
 import engine from '..';
 
